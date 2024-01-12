1bcf884f586379d12e8ac30b45d236195553c25f
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-12 23:16:27 +0530

    Create Strings.c

---
+++ C - Programming/Strings.c
@@ -0,0 +1,16 @@
+# In c no data type called string.
+# Collection of characters in c we call as string.
+# A character Array is a string it is ended with a null '\0'.
+
+Example
+/* Program to print the characters of string */
+
+#include<stdio.h>
+void main()
+{
+ char s[]="Hello";  // Initilizing the array of characters 
+ int i;
+ for(i=0;s[i]!='\0';i++)  // Rotating the loop until end of the characters in array
+ {
+   Printf("%c\t",s[i]);   // Printing the string
+ }
