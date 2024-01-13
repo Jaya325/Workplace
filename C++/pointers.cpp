701f88fb97f88c0f1bc355785cea0dcc56e8be8e
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-13 23:22:16 +0530

    Update pointers.cpp

--- C++/pointers.cpp
+++ C++/pointers.cpp
@@ -1,6 +1,5 @@
 // What is pointers
 // It is a secondary derived data type which is used to store the address of the another variable 
-
 Example:
 #include<iostream>
 using namespace std;
@@ -9,4 +8,27 @@ int main()
   int a=10,*p;
   p=&a;
   cout<<*p<<endl;
+}
+
+// Smart Pointers
+/* In c++ this pointers are used to remove the chances of memory leak. There are 3 diffrent types of smart pointers
+1. Unique pointer 
+2. Shared Pointer
+3. Week pointer */
+
+
+/* Unique pointer*/
+
+#include<iostream>
+using namespace std;
+
+class foo{
+  int x;
+public:
+  explicit foo(int x):x{x}{}
+  int getx() {return x;}
+};
+int main()
+{
+  unique_ptr<foo>p1(new foo(10));
 }
