114a2ef7e4539bcaf5ff13f7c9f9f6f3e4c733d7
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-13 13:59:38 +0530

    Update Inheritance.cpp

--- C++/Inheritance.cpp
+++ C++/Inheritance.cpp
@@ -1,2 +1,20 @@
 # Inheritance
 // It is popularly used techniq in Opps concepts for code reusability 
+// We are created a base class which contains x,y attributes 
+// Through the base class we are going to create a derived class through we create the objects 
+
+
+#include<iostream.h>
+using namespace std;
+class base
+public{
+  int x;
+  int y;
+};
+int main()
+{
+  int x,y;
+  cin>>x;
+  cin>>y;
+}
+
