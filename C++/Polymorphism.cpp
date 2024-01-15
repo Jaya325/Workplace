438a5c225a197d205fe16b383a358056fa9a96bf
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-15 23:16:23 +0530

    Update Polymorphism.cpp

--- C++/Polymorphism.cpp
+++ C++/Polymorphism.cpp
@@ -1,3 +1,27 @@
 // Polymorphism
-// Which is used to 
-// Run Time Polymorphism 
+/* It is mainly devided into the 2 types 1. Compiletime Polymorphism 2. Run time polymorphism 
+We will see the both with an eg 
+1. Compiletime Polymorphism
+-a) Function Overloading
+-b) Operator overloading
+2. Runtime Polymorphism
+- Function Overriding */
+
+/* Function overloading: We have created the multile functions with same name but diffrent parameters. Which means we are passing diffrent parameters in both the functions but we created same function name lets see some eg */
+#include<iostream>
+using namespace std;
+class base
+{
+ Public:
+ void fun(int x) {cout<<"integer value"<<endl;}
+ void fun(float x) {cout<<"Float value"<<endl;}
+};
+int main()
+{
+  base b;
+  b.fun(5);
+  b.fun(34.46);
+  return 0;
+}
+
+/* Operator Overloading :*/
