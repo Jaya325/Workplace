7414682dfab635079d5a9535d99bf5cb83b6704f
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-15 22:17:50 +0530

    Update Encapsulation.cpp

--- C++/Encapsulation.cpp
+++ C++/Encapsulation.cpp
@@ -1,10 +1,19 @@
 // Encapsulation
+/*  Encapsulation is binding together the data and functions that manipulates those data.
+- > suppose we have class in that class we have data members and member function then we can call it as encapsulation*/
 
 Eg: 
 
 #include<iostream.h>
 using namespace std;
+class base{
+  int x;
+public:
+  void setx(int a){x=a;}
+  int getx(){return x}
+};
 int main()
 {
-  //Helloworld
+  // do some code....
+  return 0;
 }
