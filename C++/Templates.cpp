0b5865ac7ccad1352413079f25131e90b4a94dcb
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-17 00:49:49 +0530

    Update Templates.cpp

--- C++/Templates.cpp
+++ C++/Templates.cpp
@@ -5,7 +5,14 @@
 
 #include<iostream>
 using namespace std;
-int main()
-{
-  cout<<"hello world"<<endl;
+template <typename T>
+T maximum(T a, T b) {
+    return (a > b) ? a : b;
+}
+int main(){
+    int intResult = maximum<int>(5, 10);
+    double doubleResult = maximum<double>(4.9, 6.8);
+    cout<<intResult<<endl;
+    cout<<doubleResult<<endl;
+    return 0;
 }
