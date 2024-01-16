c729d3e41b1fbf182309494095116c67731a0ac4
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-17 00:15:49 +0530

    Update Templates.cpp

--- C++/Templates.cpp
+++ C++/Templates.cpp
@@ -1,5 +1,7 @@
-// Temlates
-// This are used to 
+// Templates
+/* Templates allows functions and classes to operate with generic types.
+-> This will allows a function (or) class to work on many diffrent data types.
+-> This will used to over come the disadvantage of Function overloading */
 
 #include<iostream>
 using namespace std;
