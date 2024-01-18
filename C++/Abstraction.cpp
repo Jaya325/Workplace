21282951cc70a3cbcd0d2ec9a17ab6f84565e89b
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-18 23:32:57 +0530

    Create Abstraction.cpp

---
+++ C++/Abstraction.cpp
@@ -0,0 +1,31 @@
+/* Abstraction 
+-> displaying only essential information and hiding the details.
+Data abstraction – This type only shows the required information about the data and hides the unnecessary data.
+Control Abstraction – This type only shows the required information about the implementation and hides unnecessary information.
+  */
+eg:
+#include <iostream>
+using namespace std;
+class hello{
+    private:
+      int a,b;
+    public:
+     void world(int x,int y)
+    {
+      a=x;
+      b=y;
+    }
+        void show()
+    {
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    }
+
+};
+int main()
+{
+    hello h;
+    h.world(10,20);
+    h.show();
+    return 0;
+}
