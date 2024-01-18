1230dedf9a1e9c0b7d7047dedad9b79cc6675812
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-18 23:41:00 +0530

    Update Arrays.c

--- C - Programming/Arrays.c
+++ C - Programming/Arrays.c
@@ -21,14 +21,16 @@ for(i=0;i<5;i++)
 {
  scanf("%d",&arr[i]); // taking input from user 
 }
+}
 
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-int arr[5];
+int arr[5]={1,2,3,4,5};
 int i;
 for(i=0;i<5;i++)
 {
- scanf("%d",&arr[i]); // To print the array which is taken from user 
+ printf("%d",arr[i]); // To print the array which is taken from user
+}
 }
