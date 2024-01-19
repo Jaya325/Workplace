770e2ccd54fe59bab673361917a4314efa96d43e
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-01-19 23:26:39 +0530

    Update and rename C++/Polymorphism.cpp to C++/Polymorphism/Polymorphism.cpp

--- C++/Polymorphism/Polymorphism.cpp
+++ C++/Polymorphism/Polymorphism.cpp
@@ -1,14 +1,6 @@
-
-// Polymorphism
-/* It is mainly devided into the 2 types 1. Compiletime Polymorphism 2. Run time polymorphism 
-We will see the both with an eg 
-1. Compiletime Polymorphism
--a) Function Overloading
--b) Operator overloading
-2. Runtime Polymorphism
-- Function Overriding */
-
 /* Function overloading: We have created the multile functions with same name but diffrent parameters. Which means we are passing diffrent parameters in both the functions but we created same function name lets see some eg */
+Eg
+
 #include<iostream>
 using namespace std;
 class base
