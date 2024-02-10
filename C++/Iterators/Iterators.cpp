fde7f405e05bb6af2258bb50397271249ad46e42
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2024-02-10 23:12:24 +0530

    Update Iterators.cpp

--- C++/Iterators/Iterators.cpp
+++ C++/Iterators/Iterators.cpp
@@ -1,7 +1,14 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  int i;
-  return 0;
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    // Using iterators to traverse the vector
+    for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+        std::cout << *it << " "; // Dereferencing the iterator to access the element
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
