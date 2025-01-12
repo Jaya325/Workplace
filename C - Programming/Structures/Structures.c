03ba39f93adadb494c48edaed2c4a1d9a63e9dc1
Jayasimha <77492397+Jaya325@users.noreply.github.com>
2025-01-12 22:30:20 +0530

    Update Structures.c

--- C - Programming/Structures/Structures.c
+++ C - Programming/Structures/Structures.c
@@ -1,5 +1,5 @@
 // Structures
-/* it is a user defined seconady datatype which is a collection a diffrent datatypes which are in contigios memory locations */
+/* it is a user defined seconady datatype which is a collection a diffrent datatypes which are in contigious memory locations */
 
 Eg:
 #include<stdio.h>
@@ -11,7 +11,33 @@ struct st
 };
 int main()
 {
-  struct st s= {"jaya",10,98.5};
+  struct st s= {"Hello",10,98.5};
   printf("%s %d %f\n",s.name,s.age,s.marks);
 }
+
+/* Accessing the structure members by pointers */
+Eg:
+#include<stdio.h>
+struct st
+{
+ char name[10];
+ int age;
+ float marks;
+};
+int main()
+{
+  struct st *s;
+  s = (struct st *)malloc(sizeof(struct st));  // We are allocating memory in C when we are using pointer we must allocate some memory to it
+  if (s == NULL) {
+    printf("Memory allocation failed!\n");
+    return 1;  // Exit with an error code
+    }
+   strcpy(s->name,"Hello");
+   s->age = 20;
+   s->marks = 95.2;
+  printf("%s %d %f",s->name,s->age,s->marks);
+  free(s);  // We are freeing the memory which we used for structure 
+}
+
+
 
